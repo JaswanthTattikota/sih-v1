4df4b6ecdd9d5d9fe588748fedfcf4e9c7204daf
JaswanthTattikota <jaswanth.micron@gmail.com>
2023-12-14 19:28:37 +0530

    Modified app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS, cross_origin
-import cohere
+from flask import Flask, request, jsonify
 import os
+import requests
+import json
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+
 # Replace 'YOUR_COHERE_API_KEY' with your Cohere API key
 
 # Initialize the Cohere Client with an API Key
-api = os.getenv("COHERE_API_KEY")
-co = cohere.Client(api)
+api = os.getenv("GEMINI_API_KEY")
+api_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + api
 
 # @app.route('/')
 # def index():
 #     return render_template('index.html')
 
-@app.route('/get_response', methods=['POST'])
+@app.route('/', methods=['POST'])
 def get_response():
     try:
         course = request.get_json().get('course')
         target = request.get_json().get('target')
         duration = request.get_json().get('duration')
+        # module = request.get_json().get('module')
+        # submodules = request.get_json().get('submodules')
         
-        # Generate a prediction for the prompt
-        prediction = co.chat(message=f'''
-            I want to create a {course} course for {target} which has a duration of {duration}.
+        # submodules = " , ".join(submodules)
+            # Define the request payload
+        payload = {
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": 
+                            f'''
+        I want to create a {course} course for {target} which has a duration of {duration}.
             Can you create a table of contents for it?
-            Give the output in strict complete json format:
+            Give the output in json format as code block:
             {{
                 "courseTitle": "string",
                 "courseDescription": "string",
@@ -47,14 +56,36 @@ def get_response():
                     }}
                 ]
             }}
-            where quiz should only be generated for whole module and not submodules, also create quiz for each module. The quiz should cover the concepts of all the submodules present in that module
-        ''', model='command', connectors=[{"id": "web-search", "name": "Web Search", "created_at": "0001-01-01T00:00:00Z", "updated_at": "0001-01-01T00:00:00Z", "continue_on_failure": True}])
+            where quiz should only be generated for whole module and not submodules, also give quiz description for each module. The quiz description should cover the concepts of all the submodules present in that module
+        '''
+                        }
+                    ]
+                }
+            ]
+        }
 
-        print(prediction.text)
+        # Set the headers
+        headers = {
+            "Content-Type": "application/json"
+        }
 
+        # Make the API call
+        response = requests.post(api_url, json=payload, headers=headers)
 
-        # Return the predicted text in JSON format
-        return prediction.text[7:-3]
+        if response.status_code == 200:
+            # Parse and print the response JSON
+            response_json = response.json()
+            print(response_json["candidates"][0]["content"]["parts"][0]["text"][7:-3])
+            return (response_json["candidates"][0]["content"]["parts"][0]["text"][7:-3])
+            # print(json.dumps(response_json, indent=2))
+            # print(response_json)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return json.dump(
+            {
+                "msg":"Error Assessing AI"
+            }
+            ) 
 
     except Exception as e:
         return jsonify({'error': str(e)})
